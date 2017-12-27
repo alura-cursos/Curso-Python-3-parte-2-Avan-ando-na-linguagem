2c14539fa3d94bffd005482ec13f3a536757dcf0
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 07:51:11 -0200

    capitulo2

--- forca.py
+++ forca.py
@@ -5,14 +5,23 @@ def jogar():
 
     palavra_secreta = "banana"
 
-	enforcou = False
-	acertou = False
+    enforcou = False
+    acertou = False
 
-	while(not acertou and not enforcou):
-	    print("Jogando...")
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando ...")
 
     print("Fim do jogo")
-
 
 if(__name__ == "__main__"):
     jogar()
