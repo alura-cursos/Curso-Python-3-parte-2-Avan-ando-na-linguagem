3cf17e1794472ffe2f767885e4ebca971407368f
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 07:56:35 -0200

    capitulo6

--- forca.py
+++ forca.py
@@ -1,9 +1,23 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
