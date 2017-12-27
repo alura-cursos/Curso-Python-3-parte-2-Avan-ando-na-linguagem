e0a58cad96a4b173899a2d998b63871854b439d8
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 07:50:06 -0200

    capitulo1

---
+++ forca.py
@@ -0,0 +1,18 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+	enforcou = False
+	acertou = False
+
+	while(not acertou and not enforcou):
+	    print("Jogando...")
+
+    print("Fim do jogo")
+
+
+if(__name__ == "__main__"):
+    jogar()
